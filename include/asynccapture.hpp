c986bda51cd8e8e8ad18355e048705d0ace34672
djdt <10266332+djdt@users.noreply.github.com>
2026-04-04 17:20:54 +0200

    async class

---
+++ include/asynccapture.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <condition_variable>
+#include <opencv2/videoio.hpp>
+#include <thread>
+
+class AsyncVideoCapture {
+private:
+  cv::VideoCapture cap;
+  cv::Mat frame;
+  std::thread thread;
+  std::mutex mutex;
+  std::condition_variable cv;
+  std::atomic<bool> running;
+  bool frame_ready;
+
+public:
+  AsyncVideoCapture(const std::string &filename, int api);
+
+  ~AsyncVideoCapture();
+
+  void read(cv::Mat &output);
+
+  int get(const int prop);
+  void set(const int prop, const double value);
+
+private:
+  void update();
+};
