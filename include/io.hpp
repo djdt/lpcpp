a3b57ab45a4a35acf86987a57093898f21494921
djdt <10266332+djdt@users.noreply.github.com>
2026-04-16 11:50:00 +0200

    ensure output created

--- include/io.hpp
+++ include/io.hpp
@@ -20,15 +20,20 @@ template <typename Iter>
 void draw_particles_on_frame(cv::InputArray &input,
                              cv::InputOutputArray &output, Iter rbegin,
                              Iter rend, const int particle_frames) {
+
+  output.createSameSize(input, CV_8UC3);
   cv::cvtColor(input, output, cv::COLOR_GRAY2BGR);
+
   auto color = cv::Scalar(0, 0, 255);
   int decay = 255 / particle_frames;
   std::vector<std::vector<cv::Point>> contours;
+
   for (auto it = rbegin; it != rend; ++it) {
     contours.resize(it->size());
     std::transform(std::execution::par, it->begin(), it->end(),
                    contours.begin(),
                    [](const Particle &p) { return p.contour(); });
+
     cv::drawContours(output, contours, -1, color, 1.0, 8);
     color[2] -= decay;
   }
