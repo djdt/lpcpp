fb0e3e8f9ea4bb5a088728ff4dcfdd066bf5764a
djdt <10266332+djdt@users.noreply.github.com>
2026-07-20 11:36:02 +0200

    add b64 encoder to vtk output

--- src/io.cpp
+++ src/io.cpp
@@ -14,6 +14,51 @@
 #include "cpuproc.hpp"
 #include "particle.hpp"
 
+class base64_encoder {
+private:
+  std::ostream &_ostr;
+  char _buffer[3];
+  size_t _count;
+
+public:
+  explicit base64_encoder(std::ostream &ostr) : _ostr(ostr), _count(0) {}
+
+  ~base64_encoder() {
+    if (_count > 0) {
+      encode_chunk(_count);
+    }
+  }
+
+  template <typename T>
+  typename std::enable_if<std::is_arithmetic<T>::value, base64_encoder &>::type
+  operator<<(const T &val) {
+    const uchar *bytes = reinterpret_cast<const uchar *>(&val);
+    for (size_t i = 0; i < sizeof(T); ++i) {
+      _buffer[_count++] = bytes[i];
+      if (_count == 3) {
+        encode_chunk(3);
+        _count = 0;
+      }
+    }
+    return *this;
+  }
+
+private:
+  void encode_chunk(size_t bytes) {
+    static const std::string b64 =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    uchar b1 = _buffer[0];
+    uchar b2 = (bytes > 1) ? _buffer[1] : 0;
+    uchar b3 = (bytes > 2) ? _buffer[2] : 0;
+
+    _ostr << b64[(b1 >> 2) & 0x3F];
+    _ostr << b64[((b1 << 4) | (b2 >> 4)) & 0x3F];
+    _ostr << (bytes > 1 ? b64[((b2 << 2) | (b3 >> 6)) & 0x3F] : '=');
+    _ostr << (bytes > 2 ? b64[b3 & 0x3F] : '=');
+  }
+};
+
 void draw_particles_on_frame(cv::InputArray &input,
                              cv::InputOutputArray &output,
                              std::vector<Particle> &particles) {
@@ -95,9 +140,11 @@ bool save_particle_point_data_vtk(const Particle &particle,
       << " 0 " << particle.frameCount() << "\">\n";
   ofs << "\t\t\t<CellData Scalars=\"processed\">\n";
   ofs << "\t\t\t\t<DataArray type=\"Float32\" Name=\"processed\" "
-         "format=\"ascii\">\n";
+         "format=\"binary\">\n";
 
   for (size_t z = 0; z < particle.frameCount(); ++z) {
+    auto b64enc = base64_encoder(ofs);
+
     const cv::Mat &image = particle.image(z);
     cv::Rect rect = cv::boundingRect(particle.contour(z));
     cv::Point offset = rect.tl() - bounds.tl();
@@ -107,14 +154,14 @@ bool save_particle_point_data_vtk(const Particle &particle,
         size_t sx = x - offset.x;
         size_t sy = y - offset.y;
         if (sx >= 0 && sx < image.cols && sy >= 0 && sy < image.rows) {
-          ofs << image.at<float>(sy, sx) << " ";
+          b64enc << image.at<float>(sy, sx);
         } else {
-          ofs << "0 ";
+          b64enc << 0.f;
         }
       }
-      ofs << "\n";
     }
   }
+  ofs << "\n";
 
   ofs << "\t\t\t\t</DataArray>\n";
   ofs << "\t\t\t</PointData>\n";
