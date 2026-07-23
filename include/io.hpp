e4969d7e3961a08b1d31e55e77ce2d927a9351b8
djdt <10266332+djdt@users.noreply.github.com>
2026-07-23 21:23:33 +0200

    rename and make hdf5 optional

--- include/io.hpp
+++ include/io.hpp
@@ -6,26 +6,25 @@
 #include <fstream>
 #include <vector>
 
-void draw_particles_on_frame(cv::InputArray &input,
-                             cv::InputOutputArray &output,
-                             std::vector<Particle> &particles);
-
-void write_particle_header(std::ofstream &ofs);
-void write_particle_data(const std::vector<Particle> &particles,
-                         std::ofstream &ofs);
-// bool write_particle_images(const std::vector<Particle> &particles,
-//                            const std::filesystem::path &output_dir);
+// Writes the column names used in `write_particle_data` to a file
+void write_particle_properties_header(std::ofstream &ofs);
+// Exports particle parameters to a file
+void write_particle_properties(const std::vector<Particle> &particles,
+                               std::ofstream &ofs);
 
 bool save_particle_contours(const Particle &particle,
                             const std::filesystem::path &path);
 
-bool save_particle_image(const Particle &particle,
-                         const std::filesystem::path &path);
+// Saves a particles mask, processed and raw images to a png
+bool save_particle_data_png(const Particle &particle,
+                            const std::filesystem::path &path);
 
+// Saves the particles contour masks and processed images to a VTK ImageData
+// formatted text file
 bool save_particle_data_vtk(const Particle &particle,
                             const std::filesystem::path &path);
 
-#ifdef ENABLE_HDF5_EXPORT
+// Saves the particles contour masks and processed images to an VTK compatible
+// compressed HDF5 archive
 bool save_particle_data_hdf5(const Particle &particle,
                              const std::filesystem::path &path);
-#endif
--- src/io.cpp
+++ src/io.cpp
@@ -4,10 +4,8 @@
 #include "cpuproc.hpp"
 #include "particle.hpp"
 
-#include <algorithm>
 #include <fstream>
 #include <iostream>
-#include <iterator>
 #include <ostream>
 #include <vector>
 
@@ -20,33 +18,15 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 
-void draw_particles_on_frame(cv::InputArray &input,
-                             cv::InputOutputArray &output,
-                             std::vector<Particle> &particles) {
-
-  output.createSameSize(input, CV_8UC3);
-  cv::cvtColor(input, output, cv::COLOR_GRAY2BGR);
-
-  auto color = cv::Scalar(0, 0, 255);
-  std::vector<std::vector<cv::Point>> contours;
-  contours.reserve(particles.size());
-
-  std::transform(particles.begin(), particles.end(),
-                 std::back_inserter(contours),
-                 [](const Particle &p) { return p.contour(); });
-
-  cv::drawContours(output, contours, -1, color, 1.0, 8);
-}
-
-void write_particle_header(std::ofstream &ofs) {
+void write_particle_properties_header(std::ofstream &ofs) {
   ofs << "id,frame,frame_count,area,aspect,circular_equivalent_diameter,"
          "circularity,convexity,intensity,maximum_feret,minimum_feret,maximum_"
          "legendre,minimum_legendre,mean_diameter,perimeter,sharpness,x,y"
       << std::endl;
 }
 
-void write_particle_data(const std::vector<Particle> &particles,
-                         std::ofstream &ofs) {
+void write_particle_properties(const std::vector<Particle> &particles,
+                               std::ofstream &ofs) {
   cv::Mat mask;
   for (auto it = particles.begin(); it != particles.end(); ++it) {
     const std::vector<cv::Point> &contour = it->contour();
@@ -92,16 +72,15 @@ bool save_particle_contours(const Particle &particle,
   return false;
 }
 
-bool save_particle_image(const Particle &particle,
-                         const std::filesystem::path &path) {
-  auto color = cv::Scalar(0, 0, 255);
-  cv::Mat image, raw_image, mask;
+bool save_particle_data_png(const Particle &particle,
+                            const std::filesystem::path &path) {
+  cv::Mat image, mask, dst;
   particle.image().convertTo(image, CV_8U);
-  particle.rawImage().convertTo(raw_image, CV_8U);
   mask_for_contour(particle.contour(), mask);
-  const cv::Mat src[] = {raw_image, image, mask};
-  cv::Mat dst;
+
+  const cv::Mat src[] = {particle.rawImage(), image, mask};
   cv::merge(src, 3, dst);
+
   if (not cv::imwrite(path.string(), dst)) {
     std::cerr << "failed to save " << path << std::endl;
     return true;
@@ -177,9 +156,9 @@ bool save_particle_data_vtk(const Particle &particle,
   return false;
 }
 
-#ifdef ENABLE_HDF5_EXPORT
 bool save_particle_data_hdf5(const Particle &particle,
                              const std::filesystem::path &path) {
+#ifdef ENABLE_HDF5_EXPORT
   cv::Rect bounds = particle.boundingRect();
 
   std::vector<float> buf;
@@ -259,5 +238,7 @@ bool save_particle_data_hdf5(const Particle &particle,
   proc.write(buf.data(), H5::PredType::NATIVE_FLOAT);
   mask.write(mbuf.data(), H5::PredType::NATIVE_UCHAR);
   return false;
-}
+#else
+  throw "lpcpp was not compiled with HDF5 support";
 #endif
+}
